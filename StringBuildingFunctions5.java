1a713738a60262bfaa49f73784a0a0609df8030c
Laxmi Patil <laxmipatil88392@gmail.com>
2026-04-19 19:01:45 +0530

    Add files via upload

---
+++ StringBuildingFunctions5.java
@@ -0,0 +1,13 @@
+package stringfunction;
+
+public class StringBuildingFunctions5 {
+		// 5. Function to remove all whitespace from a string
+		public static String removeWhitespace(String str) {
+		return str == null ? null : str.replaceAll("\\s", "");
+		}
+
+		public static void main(String[] args) {
+		System.out.println("5. Remove Whitespace: " + removeWhitespace("Hello World"));
+		}
+		}
+
