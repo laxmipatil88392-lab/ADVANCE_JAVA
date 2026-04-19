456e04227e8ee4b745ff2208000ca21d93ba9816
Laxmi Patil <laxmipatil88392@gmail.com>
2026-04-19 18:58:04 +0530

    Update StringBuildingFunctions1.java

--- StringBuildingFunctions1.java
+++ StringBuildingFunctions1.java
@@ -1,3 +1,5 @@
+//Q1. Write a Java Program for Checking if a given string is null or contains only whitespace using user
+//defined function isNullOrEmpty().
 package stringfunction;
 
 public class StringBuildingFunctions1 {
