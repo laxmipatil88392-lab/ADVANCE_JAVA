6fe21a9fd1ee4df0b5e41b9c7a9814e3e3e69870
Laxmi Patil <laxmipatil88392@gmail.com>
2026-04-19 12:50:14 +0530

    Update InsertAtEndDemo.java

--- InsertAtEndDemo.java
+++ InsertAtEndDemo.java
@@ -1,3 +1,5 @@
+//3. Write a Java program to insert the specified element at the end of a linked list.( using
+//l_listobj.offerLast(&quot;Pink&quot;))
 package LinkedList;
 import java.util.*;
 public class InsertAtEndDemo {
