fc794476f1e8c66d88511789a0d6f35d62b9e423
Laxmi Patil <laxmipatil88392@gmail.com>
2026-04-19 18:58:31 +0530

    Add files via upload

---
+++ StringBuildingFunctions2.java
@@ -0,0 +1,17 @@
+package stringfunction;
+
+public class StringBuildingFunctions2 {
+		// 2. Function to count occurrences of a substring
+		public static int countOccurrences(String mainString, String subString) {
+		if (mainString == null || subString == null || subString.isEmpty()) {
+		return 0;
+		}
+		return (mainString.length() - mainString.replace(subString, "").length()) /
+		subString.length();
+		}
+
+		public static void main(String[] args) {
+		System.out.println("2. Occurrences: " + countOccurrences("hello hello world", "hello"));
+		}
+		}
+
