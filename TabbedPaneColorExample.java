a5edd33f237ebb2fb35ab968a16e23cd3d3d9909
Laxmi Patil <laxmipatil88392@gmail.com>
2026-04-19 18:32:15 +0530

    Update TabbedPaneColorExample.java

--- TabbedPaneColorExample.java
+++ TabbedPaneColorExample.java
@@ -1,3 +1,5 @@
+//5d. Develop a Swing program in Java to create a Tabbed Pan of Cyan, Magenta and Yellow and
+//display the concerned color whenever the specific tab is selected in the Pan
 package Swing_programs;
 
 import java.awt.Color;
