ae0ff681701fdfd89584661df605edaf92c599e1
Laxmi Patil <laxmipatil88392@gmail.com>
2026-04-19 19:00:47 +0530

    Add files via upload

---
+++ StringBuildingFunctions4.java
@@ -0,0 +1,18 @@
+package stringfunction;
+
+public class StringBuildingFunctions4 {
+		// 4. Function to check if a string is a palindrome
+		public static boolean isPalindrome(String str) {
+		if (str == null) {
+		return false;
+		}
+		String cleaned = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+		return cleaned.equals(new StringBuilder(cleaned).reverse().toString());
+		}
+
+		public static void main(String[] args) {
+		System.out.println("4. Palindrome: " + isPalindrome("A man, a plan, a canal: Panama"));
+		}
+		}
+
+
