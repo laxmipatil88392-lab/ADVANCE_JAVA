812e37fec2015dfa5c53f801918abe61f436de29
Laxmi Patil <laxmipatil88392@gmail.com>
2026-04-19 19:05:03 +0530

    Add files via upload

---
+++ StringBuildingFunctions8.java
@@ -0,0 +1,16 @@
+package stringfunction;
+
+public class StringBuildingFunctions8 {
+		// 8. Function to check if a string contains only digits
+		public static boolean isNumeric(String str) {
+		if (str == null || str.isEmpty()) {
+		return false;
+		}
+		return str.matches("\\d+");
+		}
+
+		public static void main(String[] args) {
+		System.out.println("8. Is Numeric: " + isNumeric("12345"));
+		}
+		}
+
