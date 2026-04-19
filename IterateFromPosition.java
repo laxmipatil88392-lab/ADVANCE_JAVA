2baffee6dd0ecc96cbd92283f851a8db3d8822df
Laxmi Patil <laxmipatil88392@gmail.com>
2026-04-19 12:39:03 +0530

    Update IterateFromPosition.java

--- IterateFromPosition.java
+++ IterateFromPosition.java
@@ -1,3 +1,5 @@
+//1. Write a Java program to iterate through all elements in a linked list starting at the
+//specified position ( 2 nd ) using iterator ( hint : Iterator iteratorobj = listobj.listIterator(1))
 package LinkedList;
 import java.util.*;
 public class IterateFromPosition {
