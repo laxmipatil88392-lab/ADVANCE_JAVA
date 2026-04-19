3c37c2f02d6aa9aa107298d0a387fcbd6b409301
Laxmi Patil <laxmipatil88392@gmail.com>
2026-04-19 18:27:45 +0530

    Update TabbedPaneExample.java

--- TabbedPaneExample.java
+++ TabbedPaneExample.java
@@ -1,3 +1,5 @@
+//5b. Develop a Swing program in Java to create a Tabbed Pan of RED, BLUE and GREEN and display
+//the concerned color whenever the specific tab is selected in the Pan.
 package Swing_programs;
 import java.awt.Color;
 import javax.swing.*;
