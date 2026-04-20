89cea87159fba00e4b2d359ded6af860e2480f9e
Laxmi Patil <laxmipatil88392@gmail.com>
2026-04-20 20:44:29 +0530

    Update FactorialServlet.java

--- src/main/java/com/factorialsevlet/FactorialServlet.java
+++ src/main/java/com/factorialsevlet/FactorialServlet.java
@@ -1,3 +1,7 @@
+//7a. Build a Session Management using JSP program set with one minute session expiry time to
+//get your name through text box and press submit to display the message by greeting Hello your
+//name!. Check the expiry of the session after one minute.
+
 package com.factorialsevlet;
 import java.io.IOException;
 import java.io.PrintWriter;
