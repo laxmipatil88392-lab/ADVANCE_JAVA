f4f712591914e3709c2fed4ef1159cd849b713f3
Laxmi Patil <4al24cs113@aiet.org.in>
2026-04-24 19:56:24 +0530

    commit

---
+++ src/main/java/com/mysql/Mysql_Update.java
@@ -0,0 +1,77 @@
+package com.mysql;
+import java.sql.*;
+import java.util.Properties;
+
+public class Mysql_Update
+{
+public static void main(String[] args)
+{
+try
+{
+Connection dbConnection = null;
+
+try {
+String url = "jdbc:mysql://localhost/test";
+Properties info = new Properties();
+info.put("user", "root");
+info.put("password", "Laxmi@2006");
+
+dbConnection = DriverManager.getConnection(url, info);
+
+if (dbConnection != null)
+{
+System.out.println("Successfully connected to MySQL database test");
+}
+}
+catch (SQLException ex)
+{
+System.out.println("An error occurred while connecting MySQL databse");
+ex.printStackTrace();
+}
+
+// SQL SELECT query
+String query = "SELECT * FROM coffee";
+
+// create the java statement
+Statement st = dbConnection.createStatement();
+
+// execute the query
+ResultSet rs = st.executeQuery(query);
+
+// display records
+while (rs.next())
+{
+int id = rs.getInt("id");
+String coffee_name = rs.getString("coffee_name");
+int price = rs.getInt("price");
+
+System.out.format("\n%d %s %d", id, coffee_name, price);
+}
+rs.close();
+
+// updating records
+String query2 = "update coffee set price=950 where id=102";
+PreparedStatement preparedStmt1 = dbConnection.prepareStatement(query2);
+preparedStmt1.executeUpdate();
+preparedStmt1.close();
+
+// display after update
+ResultSet rs2 = st.executeQuery("SELECT * FROM coffee");
+
+while (rs2.next())
+{
+int id = rs2.getInt("id");
+String coffee_name = rs2.getString("coffee_name");
+int price = rs2.getInt("price");
+
+System.out.format("\n%d %s %d", id, coffee_name, price);
+}
+
+}
+catch (Exception e)
+{
+System.err.println("Got an exception! ");
+System.err.println(e.getMessage());
+}
+}
+}
